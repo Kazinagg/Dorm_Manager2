d61e99ca1e18bc812287bdcdd841df8ab99c3b03
Kazinagg <artemfrolov04@gmail.com>
2023-06-27 01:02:19 +0300

    табличка

--- dorm_manager2/src/pages/HomePage.tsx
+++ dorm_manager2/src/pages/HomePage.tsx
@@ -1,11 +1,44 @@
+// pages/HomePage.tsx
 import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import './HomePage.css'; // Импортируем файл стилей
 
 const HomePage: React.FC = () => {
-  // Здесь вы мб использовать useEffect для получения данных о студентах и отображения их в таблице
+  // Здесь вы можете использовать useEffect для получения данных о студентах и отображения их в таблице
+
+  // Для демонстрации создадим фиктивные данные
+  const rows = [
+    { name: 'Иван Иванов', room: '101' },
+    { name: 'Петр Петров', room: '102' },
+    // добавьте больше студентов здесь...
+  ];
+
   return (
-    <div>
-      <h1>Добро пожаловать на главную страницу</h1>
-      {/* Здесь будет таблица со студентами */}
+    <div className="home-page">
+      <h1 className="home-page__title">Добро пожаловать на главную страницу</h1>
+      <p className="home-page__info">
+        Жители этого общажного ада.
+      </p>
+      <TableContainer component={Paper}>
+        <Table className="home-page__table" aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Имя студента</TableCell>
+              <TableCell align="right">Номер комнаты</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.room}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };

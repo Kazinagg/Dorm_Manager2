10ee2c1629f4779d333dc1ff7d554eedfa2669cc
Kazinagg <artemfrolov04@gmail.com>
2023-06-30 16:16:47 +0300

    чистка

--- manager_dorm2/dorm_manager_/Dorm_manager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DormManagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Dorm_manager'
--- manager_dorm2/dorm_manager_/Dorm_manager/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-from .models import YourModel
-
-def get_data(request):
-    data = list(YourModel.objects.values())
-    return JsonResponse(data, safe=False)
-
-
